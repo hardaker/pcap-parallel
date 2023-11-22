a70915e1b1aebcaf6beb65deb7041855ae8da22e
Wes Hardaker <opensource@hardakers.net>
2023-11-22 14:37:02 -0800

    fix package name

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="pcap-splitter",
+    name="pcap-parallel",
     version="0.1",
     author="Wes Hardaker",
     author_email="opensource@hardakers.net",
     description="A tool for processing pcap files in parallel",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/hardaker/pcap-splitter",
+    url="https://github.com/hardaker/pcap-parallel",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
